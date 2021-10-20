250485cd6b68413112825bee1d035a43543a9380
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-10-20 22:52:50 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import normalizedMovies from  "./normalized-movies";
+import Header from "./components/header/Header";
+import Filter from "./components/filter/Filter";
+import MovieCard from "./components/movieCard/MovieCard";
+const movies = normalizedMovies.slice(0,10);
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div>
+      <Header />
+        <main>
+          <div className="container-xl">
+            <div className="row">
+              <div className="col-md-4 mb-4">
+                <div className="container">
+
+                  <Filter />
+
+                  <section className="mb-4">
+                    <h2>Bookmarked movies</h2>
+                    <ul className="bookmarked-movies list-group" />
+                  </section>
+                </div>
+              </div>
+              <section className="col-md-8">
+                <h2 className="h5">Search results</h2>
+                <div className="alert alert-info">Use the form on the left to search for a movie</div>
+                <ul className="search-results row list-unstyled">
+                  {movies.map((movie) => <MovieCard {...movie} />)}
+                </ul>
+              </section>
+            </div>{/* row */}
+          </div>{/* container */}
+        </main>
+
+      </div>
+
+    </>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+// COMPONENT SHOULD BE WRITTEN CAPITL LETETER
+function Header() {
+  return (
+    <header className="py-3 mb-2 text-center">
+      <div className="container">
+        <h1 className="h3 mb-0">Movies catalog</h1>
+      </div>
+    </header>
+  );
+}
+
+
+export default Header;
